4f9f60c32966b072c183314db5b9a3d8d7c68cd6
kevin Gutierrez <gutierrezvinicius9@gmail.com>
2022-11-02 01:58:56 -0600

    se agrego la funcionalidad de Alumnos

--- CapaDatos/Alumnos.cs
+++ CapaDatos/Alumnos.cs
@@ -5,33 +5,45 @@ using System.Data;
 using System.Data.SqlClient;
 using System.Linq;
 
-
-
 namespace CapaDatos
 {
    public class Alumnos
     {
-
+        //instanciar la clase conexion a la db
         private conexionDB conexion = new conexionDB();
 
-
+        // intanciar los comandos sqlClient para poder utilizar las funciones
         DataTable tabla = new DataTable();
-
         SqlCommand comando = new SqlCommand();
-
         public DataTable Mostrar()
         {
+            //Se abre la cadena de conexion con la clase.
             comando.Connection = conexion.Abrir();
-            comando.CommandText = "select *from prueba1";
-
+            // se llama a la consulta o el procedimiento almacenado
+            comando.CommandText = "MostrarAlumnos";
+            // se especifica el tipo de comando si es text o procedure
+            comando.CommandType = CommandType.StoredProcedure;
+            // comienza a leer todo lo que hay en la db y si coincide con el procedure o en la consulta todo ok
             SqlDataReader reader = comando.ExecuteReader();
-
+            // si la tabla esta cargada con los datos leidos todo ok.
             tabla.Load(reader);
 
+            //cerramos la conexion
             conexion.Cerrar();
             return tabla;
         }
 
-
-    }
+        // en este metodo se le pasa todos los atributos para que coincidan con los de la db (en orden a como estan en la tabla por ejemplo si es varchar tiene que ser string)
+        public void Agregar_Alumnos(string cod ,string nom1, string nom2, string ape1, string ape2, string fecha , string nomRes, string colegio, string grado, string domiPersona, string evaluacion, int DeparId, int MuniId)
+        {
+            //abrimos la conexion
+            comando.Connection = conexion.Abrir();
+            // aca hacemos el transact sql (que es la consulta o el procedimiento almacenado)
+            comando.CommandText = "insert into Alumnos values ('"+cod+"','"+nom1+"','"+nom2+"','"+ape1+"','"+ape2+"', '"+fecha+"','"+nomRes+"','"+colegio+"', '"+grado+"','"+domiPersona+"','"+evaluacion+"',"+DeparId+","+MuniId+")";
+            // aca especificamos el tipo de comando para indicar si es consulta o procedimiento almacenado
+            comando.CommandType = CommandType.Text;
+            //operación para ejecutar cualquier instrucción SQL arbitraria en SQL Server si no desea que se devuelva ningún conjunto de resultados.
+            comando.ExecuteNonQuery();
+        }
+   }
 }
--- CapaNegocio/DatosAlumnos.cs
+++ CapaNegocio/DatosAlumnos.cs
@@ -4,21 +4,24 @@ using System.Text;
 using System.Data;
 using System.Data.SqlClient;
 using CapaDatos;
-
-
-namespace CapaNegocio 
+namespace CapaNegocio
 {
     public class DatosAlumnos
     {
+        // se hace una instancia de la clase alumno que esta ubicada en la capa de datos
         private Alumnos obj = new Alumnos();
-
+        // con este metodo se puede mostrar todo los registros que estan en la consulta o prodecimiento que se detallo en la capa de datos
         public DataTable Mostrar_completo()
         {
-
+            // instanceamos la clase datatable
             DataTable tabla = new DataTable();
             tabla = obj.Mostrar();
             return tabla;
         }
-
+        // agregamos los registros esto ya es para enviarselo mas facil a la capa de presentacion
+        public void AgregarAlumno(string cod, string nom1, string nom2, string ape1, string ape2, string fecha, string nomRes, string colegio, string grado, string domiPersona, string evaluacion, string DeparId, string MuniId)
+        {
+          obj.Agregar_Alumnos(cod, nom1,  nom2,  ape1,  ape2, fecha ,  nomRes,  colegio,  grado, domiPersona, evaluacion, Convert.ToInt32(DeparId),  Convert.ToInt32(MuniId));
+        }
     }
 }
